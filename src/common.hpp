cbd56ea365526d0d2d60bf15e3dc2869ca1ed6ed
Sahin Habermann <sahino1996@hotmail.de>
2018-01-01 17:07:39 +0100

    typo during merge

--- src/common.hpp
+++ src/common.hpp
@@ -42,15 +42,14 @@ namespace voli {
 		std::string lolUsername;
 		std::string lolPassword;
 		std::string lolRegion;
-		std::string currentStatus
+		std::string currentStatus;
+		int queue;
+		bool autoplay;
         std::string auth;
         lol::WssClient wss;
         lol::HttpsClient https;
         lol::HttpsClient httpsa;
         uint32_t id;
-        int queue;
-        bool autoplay;
-        using lol::LeagueClient::LeagueClient;
         json trashbin;
 
         LeagueInstance(const LeagueInstance&) = delete;
